12bdaea39ceb68f41d00e43257772f5ba40a92e0
gautambrothersroyals-beep <gautambrothersroyals@gmail.com>
2026-02-14 23:59:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function openLogin() {
+    document.getElementById("loginBox").style.display = "block";
+}
+
+function closeLogin() {
+    document.getElementById("loginBox").style.display = "none";
+}
+function searchProduct() {
+    let input = document.getElementById("searchInput").value.toLowerCase();
+    let cards = document.getElementsByClassName("card");
+
+    for (let i = 0; i < cards.length; i++) {
+        let title = cards[i].getElementsByTagName("h3")[0].innerText.toLowerCase();
+
+        if (title.includes(input)) {
+            cards[i].style.display = "block";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
+
